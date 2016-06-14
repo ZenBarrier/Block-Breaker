ebe61be81ffc8d31ae43ea7131bdc1f6be494c43
ZenBarrier <anthony.j.barrera@gmail.com>
2016-06-14 17:57:08 -0400

    Music Player Duplication Fixed
    created static instance holder to destroy any new music player
    instances.

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -4,8 +4,18 @@ using System.Collections;
 public class MusicPlayer : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-	
+    static MusicPlayer instance = null;
+    void Start()
+    {
+        if (instance != null)
+        {
+            GameObject.Destroy(gameObject);
+            Debug.Log("Extra music player destroyed.");
+        }
+        else {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
 	}
 	
 	// Update is called once per frame

1edc6b2194d0a26f5967ac5662f7054ce2a342ab
ZenBarrier <anthony.j.barrera@gmail.com>
2016-06-15 19:09:11 -0400

    Movable Paddle
    added script to move paddle with mouse.

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float blockPosition = Input.mousePosition.x / Screen.width * 16;
+        blockPosition = Mathf.Clamp(blockPosition, 0.5f, 15.5f);
+
+        Vector3 pos = new Vector3(blockPosition, transform.position.y, 0);
+
+        transform.position = pos;
+	}
+}

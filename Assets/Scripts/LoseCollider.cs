ccfebd068e0f1382dd4c506fede2ddf9ffa2e442
ZenBarrier <anthony.j.barrera@gmail.com>
2016-06-14 20:44:17 -0400

    Trigger to lose game.
    OnTriggerEnter2D for ball fall detection.

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+    public LevelManager levelManager;
+
+	void OnTriggerEnter2D(Collider2D trigger)
+    {
+        Debug.Log("Trigger");
+        levelManager.LoadLevel("Win");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision");
+    }
+}

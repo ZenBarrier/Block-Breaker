ef0e3149beb6649f85ee68c6f503741aa29f1160
ZenBarrier <anthony.j.barrera@gmail.com>
2016-06-15 21:56:56 -0400

    Prefab multiple brick types

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+    public int maxHit;
+
+    private int timesHit;
+
+	// Use this for initialization
+	void Start () {
+        timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D colision)
+    {
+        timesHit++;
+    }
+}

439a22d3b6fde271efa22e219bf18c58b849a897
ZenBarrier <anthony.j.barrera@gmail.com>
2016-06-17 22:40:30 -0400

    Paddle Autoplay
    paddle can be set to play automatically to test game level.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,18 +3,39 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public bool autoPlay = false;
+
+    private Ball ball;
+
+    void Start()
+    {
+        ball = FindObjectOfType<Ball>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        if (!autoPlay)
+        {
+            moveWithMouse();
+        }
+        else
+        {
+            AutoPlay();
+        }
+	}
+
+    void AutoPlay()
+    {
+        Vector2 ballPosition = ball.transform.position;
+        Vector3 pos = new Vector3(ballPosition.x, transform.position.y, 0);
+        transform.position = pos;
+    }
+
+    void moveWithMouse()
+    {
         float blockPosition = Input.mousePosition.x / Screen.width * 16;
         blockPosition = Mathf.Clamp(blockPosition, 0.5f, 15.5f);
-
         Vector3 pos = new Vector3(blockPosition, transform.position.y, 0);
-
         transform.position = pos;
-	}
+    }
 }
